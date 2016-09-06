3adef0aa20c9315c059a369aed64c304618f16fe
Johannes Rieken <johannes.rieken@gmail.com>
2016-09-06 10:13:36 +0200

    add event for garbage collection

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@
 
 'use strict';
 
+import { EventEmitter } from 'events';
+import { ok } from 'assert';
+
 const _gcsignals = require('../build/Release/gcsignals');
 
 export interface GCSignal { };
@@ -15,8 +18,30 @@ export interface GCSignal { };
  */
 export const GCSignal: { new (id: number): GCSignal } = _gcsignals.GCSignal;
 
+const _emitter = new EventEmitter();
+
 /**
  * Consume ids of garbage collected signals.
  */
-export const consumeSignals: { (): number }[] = _gcsignals.consumeSignals;
+export function consumeSignals(): number[] {
+    const signals = Object.freeze(_gcsignals.consumeSignals());
+    if (signals.length > 0) {
+        _emitter.emit('gc', signals);
+    }
+    return signals;
+}
 
+export function onDidGarbageCollectSignals(callback: (ids: number[]) => any): { dispose(): void } {
+    ok(typeof callback === 'function');
+
+    _emitter.addListener('gc', callback);
+
+    return {
+        dispose() {
+            if (callback) {
+                _emitter.removeListener('gc', callback);
+                callback = null;
+            }
+        }
+    }
+}
